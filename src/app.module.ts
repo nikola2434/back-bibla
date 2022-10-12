ec76c8f2892bf1efb9188e9f86e1a968ca9412f5
Nikolay <reviakin223@gmail.com>
2022-10-12 11:17:03 +0300

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { getMongoConfig } from './config/mongo.config';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
